0127fa09e345b7c582255c637e593f76dc9585ef
achaisa <achaisa@unsa.edu.pe>
2024-08-06 22:33:35 -0500

    Variables

--- holamundo.cpp
+++ holamundo.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
 int main(){
-    std::cout << "Hola mundo";
-
-    return 0;
+    int entero = 2;
+    double doble = 3.2;
+    char caracter = 'A';
+    bool booleano = true;
+    std::string cadena = "Cadena de texto";
+    std::cout << entero << doble << caracter << booleano << cadena;
+    return  0;
 }
