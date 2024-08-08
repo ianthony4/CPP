71caf210c758ca1bbd1728bc25d3ab3482245adb
achaisa <achaisa@unsa.edu.pe>
2024-08-08 10:33:29 -0500

    cambio de tipo de datos

---
+++ operadores.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+    int estudiantes = 20;
+    estudiantes++;
+    std::cout << estudiantes;
+    return 0;
+}
---
+++ tipoDato.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+
+    int x = 10;
+    double resultado = 8 / (double) x * 100; 
+    std::cout << resultado << "%";
+
+    return 0;
+}
